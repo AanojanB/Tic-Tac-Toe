5e88624cc2b34b7d48df9572a05707adf34256a5
AanojanB <aanojan.bharathkumar@student.tdsb.on.ca>
2024-05-02 10:51:39 -0400

    Input X's and O's

--- src/App.java
+++ src/App.java
@@ -9,24 +9,28 @@ public class App extends JFrame{
     {{new JButton("Space"), new JButton("Space"), new JButton("Space")}, 
     {new JButton("Space"), new JButton("Space"),new JButton("Space")}, 
     {new JButton("Space"), new JButton("Space"),new JButton("Space")}};
-
-    String[][] playerMove = {{"0,0","0,1","0,2"},
-                            {"1,0","1,1","1,2"},
-                            {"2,0","2,1","2,2"}}; 
+    
+    String selectiveChar = "X";
 
     App(){            
-        
         for(int i = 0; i<3; i++){
             for(int j = 0; j<3; j++ ){
                 gridArea[i][j].setBounds(i*100+100, j*100+100, 100, 100);
-                add(gridArea[i][j]);
+                JButton gameInput = gridArea[i][j];
+                add(gameInput);
                 gridArea[i][j].addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent ae) {
-                        if (!textField.getText()){
-
+                        //if (gameInput.getText().equals("X") && !gameInput.getText().equals("O")){
+                            gameInput.setText(selectiveChar);
+                            if(selectiveChar == "X"){
+                                selectiveChar = "O";                            
+                            }
+                            else if (selectiveChar == "O"){
+                                selectiveChar = "X";
+                            }
                         }
                     }
-                }
+               // }
 
                 );
             }
@@ -36,13 +40,6 @@ public class App extends JFrame{
         setVisible(true);
     }
     public static void main(String[] args) throws Exception {
-        App app = new App();
-        while(true){
-            for(int i = 0; i<3; i++){
-                for(int j = 0; j<3; j++ ){
-                    app.gridArea[i][j].setText(app.playerMove[i][j]);
-                }
-            }
-        }
+        App app = new App();     
     }
 }
